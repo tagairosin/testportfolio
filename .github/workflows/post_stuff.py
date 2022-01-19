2cd693f76b2e4cb2afbe2e2ccb51494b7b1adf3f
tagairosin <69363583+tagairosin@users.noreply.github.com>
2022-01-19 11:00:19 -0800

    Update post_stuff.py

--- .github/workflows/post_stuff.py
+++ .github/workflows/post_stuff.py
@@ -1,14 +1,16 @@
 import os
+import requests
 from github import Github
 
 def main():
-    this_repo = os.environ.get('GITHUB_REPOSITORY')
-    print(this_repo)
-    g = Github()
-    repo = g.get_repo(this_repo)
-    print(repo.full_name)
-    for tag in repo.get_tags():
-        print(tag.name)
-    
+    # this_repo = os.environ.get('GITHUB_REPOSITORY')
+    # print(this_repo)
+    # g = Github()
+    # repo = g.get_repo(this_repo)
+    # print(repo.full_name)
+    # for tag in repo.get_tags():
+    #     print(tag.name)
+    r=requests.get("https://api.github.com/repos/tagairosin/testportfolio/releases")
+    print(r.json())
 if __name__ == "__main__":
     main()
